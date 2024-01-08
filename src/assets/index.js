49c01d0a83a815369224bc0adb4dc18b7e105e58
ammarjavaid <ammarjavaid014@gmail.com>
2024-01-08 17:11:13 +0500

    commit

---
+++ src/assets/index.js
@@ -0,0 +1,95 @@
+export { default as Truckinlink } from "./images/Truckinlink.png";
+export { default as Secondlookortho } from "./images/Secondlookortho.png";
+export { default as Massimomotor } from "./images/Massimomotor.png";
+export { default as Iglu } from "./images/Iglu.png";
+export { default as FMCP } from "./images/FMCP.png";
+export { default as Blink } from "./images/Blink.png";
+export { default as Atiba } from "./images/Atiba.png";
+export { default as Alamulaka } from "./images/Alamulaka.png";
+export { default as ACO1 } from "./images/ACO1.png";
+export { default as Academify } from "./images/Academify.png";
+
+// export { default as zain } from "./images/zain.png";
+export { default as zain } from "./images/zain.svg";
+// export { default as umair } from "./images/umair.svg";
+export { default as umair } from "./images/umair.png";
+export { default as raza } from "./images/raza.png";
+
+export { default as zee } from "./images/zee.png";
+export { default as zeeSign } from "./images/zeeSign.png";
+
+export { default as heroTwo } from "./images/heroPNG.png";
+
+export { default as bOne } from "./images/b1.svg";
+export { default as bTwo } from "./images/b2.svg";
+export { default as bThree } from "./images/b3.svg";
+export { default as bFour } from "./images/b4.svg";
+export { default as bFive } from "./images/b5.svg";
+export { default as bSix } from "./images/b6.svg";
+export { default as bSeven } from "./images/b7.svg";
+
+export { default as mbOne } from "./images/mb11.svg";
+export { default as mbTwo } from "./images/mb22.svg";
+export { default as mbThree } from "./images/mb33.svg";
+export { default as mbFour } from "./images/mb44.svg";
+export { default as mbFive } from "./images/mb55.svg";
+export { default as mbSix } from "./images/mb66.svg";
+export { default as mbSeven } from "./images/mb77.svg";
+
+export { default as al } from "./images/al.jpeg";
+export { default as tr } from "./images/tr.jpeg";
+export { default as mon } from "./images/mon.jpeg";
+
+export { default as videoTwo } from "./images/videoTwo.mp4";
+
+// gallery images
+export { default as galleryimg1 } from "./gallery-images/gallery-img-1.jpg";
+export { default as galleryimg2 } from "./gallery-images/gallery-img-2.jpg";
+export { default as galleryimg3 } from "./gallery-images/gallery-img-3.jpg";
+export { default as galleryimg4 } from "./gallery-images/gallery-img-4.jpg";
+
+export { default as galleryimg5 } from "./gallery-images/img-gallery-5.jpg";
+export { default as galleryimg6 } from "./gallery-images/img-gallery-6.jpg";
+export { default as galleryimg7 } from "./gallery-images/img-gallery-7.jpg";
+export { default as galleryimg8 } from "./gallery-images/img-gallery-8.jpg";
+export { default as galleryimg9 } from "./gallery-images/img-gallery-9.jpg";
+
+// tour images
+export { default as galleryTourimg1 } from "./gallery-images/tour-img-1.jpg";
+export { default as galleryTourimg2 } from "./gallery-images/tour-img-2.jpg";
+export { default as galleryTourimg3 } from "./gallery-images/tour-img-3.jpg";
+export { default as galleryTourimg4 } from "./gallery-images/tour-img-4.jpg";
+export { default as galleryTourimg5 } from "./gallery-images/tour-img-5.jpg";
+export { default as galleryTourimg6 } from "./gallery-images/tour-img-6.jpg";
+export { default as galleryTourimg7 } from "./gallery-images/tour-img-7.jpg";
+export { default as galleryTourimg8 } from "./gallery-images/tour-img-8.jpg";
+export { default as galleryTourimg9 } from "./gallery-images/tour-img-9.jpg";
+
+// games images
+
+export { default as galleryGamesimg1 } from "./gallery-images/game-img-1.jpg";
+export { default as galleryGamesimg2 } from "./gallery-images/game-img-2.jpg";
+export { default as galleryGamesimg3 } from "./gallery-images/game-img-3.jpg";
+export { default as galleryGamesimg4 } from "./gallery-images/game-img-4.jpg";
+export { default as galleryGamesimg5 } from "./gallery-images/game-img-5.jpg";
+export { default as galleryGamesimg6 } from "./gallery-images/game-img-6.jpg";
+
+// webwrite images
+
+export { default as webwrite1 } from "./gallery-images/webwrite-img-1.jpg";
+export { default as webwrite2 } from "./gallery-images/webwrite-img-2.jpg";
+// export { default as webwrite3 } from "./gallery-images/webwrite-img-3.jpg";
+export { default as webwrite3 } from "./gallery-images/tour-img-1.jpg";
+export { default as webwrite4 } from "./gallery-images/webwrite-img-4.jpg";
+export { default as webwrite5 } from "./gallery-images/webwrite-img-5.jpg";
+export { default as webwrite6 } from "./gallery-images/webwrite-img-6.jpg";
+export { default as webwrite7 } from "./gallery-images/webwrite-img-7.jpg";
+export { default as webwrite8 } from "./gallery-images/webwrite-img-8.jpg";
+export { default as webwrite9 } from "./gallery-images/webwrite-img-9.jpg";
+export { default as webwrite10 } from "./gallery-images/webwrite-img-10.jpg";
+export { default as webwrite11 } from "./gallery-images/webwrite-img-11.jpg";
+export { default as webwrite12 } from "./gallery-images/webwrite-img-12.png";
+export { default as webwrite13 } from "./gallery-images/webwrite-img-13.png";
+export { default as webwrite14 } from "./gallery-images/webwrite-img-14.png";
+export { default as webwrite15 } from "./gallery-images/webwrite-img-15.png";
+export { default as webwrite16 } from "./gallery-images/webwrite-img-16.png";
